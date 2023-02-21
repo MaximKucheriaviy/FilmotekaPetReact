0500e8ab216f0714caf36084d0825b3816459ba5
Maxim Kucheriaviy <maxim-k@i.ua>
2023-02-21 09:20:48 +0200

    card styled

--- src/components/filmList/FilmListStyled.jsx
+++ src/components/filmList/FilmListStyled.jsx
@@ -3,5 +3,12 @@ import styled from "styled-components"
 export const List = styled.div`
     display: grid;
     grid-gap: 10px;
+    width: 100%;
     grid-template-columns: 280px;
+    @media screen and (min-width: 768px) {
+        grid-template-columns: 336px 336px;
+    }
+    @media screen and (min-width: 1280px) {
+        grid-template-columns: 395px 395px 395px;
+    }
 `
--- src/components/filmList/filmCard/FilmCardStyled.jsx
+++ src/components/filmList/filmCard/FilmCardStyled.jsx
@@ -1,14 +1,36 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  outline: 2px solid red;
   width: 280px;
   height: 443px;
+  box-shadow: none;
+  border-radius: 5px;
 
+  transition-duration: 250ms;
+  transition-property: box-shadow;
+
+  &:hover{
+    box-shadow: 5px 5px 10px black;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 336px;
+    height: 495px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 395px;
+    height: 634px;
+  }
   & .imageThumb {
     height: 402px;
     overflow: hidden;
     border-radius: 5px;
+    @media screen and (min-width: 768px) {
+      height: 455px;
+    }
+    @media screen and (min-width: 768px) {
+      height: 574px;
+    }
   }
 
   & h3 {
@@ -22,6 +44,15 @@ export const Card = styled.div`
     text-transform: uppercase;
 
     color: #000000;
+
+    @media screen and (min-width: 768px) {
+      line-height: 16px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      font-size: 20px;
+      line-height: 24px;
+    }
   }
 
   & p {
@@ -32,5 +63,14 @@ export const Card = styled.div`
     line-height: 16px;
 
     color: #ff6b08;
+
+    @media screen and (min-width: 768px) {
+      line-height: 16px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      font-size: 20px;
+      line-height: 24px;
+    }
   }
 `;
